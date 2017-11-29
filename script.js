696e2137694c4dbd5d695ba8009f9c075f61b007
Koen Sparreboom <koensparreboom@gmail.com>
2017-11-29 22:35:28 +0100

    Added skills & references

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 $(document).ready(function () {
-    $("#flip").click(function () {
-        $("#panel").slideToggle("medium");
+    $("#yearOneFlip").click(function () {
+        $("#yearOnePanel").slideToggle("medium");
+    });
 
-        /*if ($("#arrow").css("transform") == "matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)")
-            $("#arrow").css("transform", "rotate(-90deg)");
-        else
-            $("#arrow").css("transform", "rotate(90deg)");*/
+    $("#yearTwoFlip").click(function () {
+        $("#yearTwoPanel").slideToggle("medium");
     });
 });
