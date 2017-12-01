3607b243894853a627adbfee5ead6635adfb6bb4
Koen Sparreboom <koensparreboom@gmail.com>
2017-12-01 22:47:44 +0100

    Added header links, added language skills & put contact links in the middle container

--- script.js
+++ script.js
@@ -6,4 +6,28 @@ $(document).ready(function () {
     $("#yearTwoFlip").click(function () {
         $("#yearTwoPanel").slideToggle("medium");
     });
+
+    $("#top-button").hide();
+    $(function toTop() {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#top-button').fadeIn("medium");
+            } else {
+                $('#top-button').fadeOut("medium");
+            }
+        });
+
+        $('#top-button').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 400);
+            return false;
+        });
+    });
 });
+
+function scrollToId(id, speed = 400) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, speed);
+}
