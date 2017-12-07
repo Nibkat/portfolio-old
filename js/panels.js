b6e204ecd6ebe5ea0f7ae58529c514a33717afa2
Koen Sparreboom <koensparreboom@gmail.com>
2017-12-07 16:44:19 +0100

    Improved responsiveness

--- js/panels.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#yearOneFlip").click(function () {
-        $("#yearOnePanel").slideToggle("medium");
-    });
-
-    $("#yearTwoFlip").click(function () {
-        $("#yearTwoPanel").slideToggle("medium");
-    });
-});
